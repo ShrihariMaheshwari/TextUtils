6469bb2a55ce5a88ae4e0f710a37d4c79da0aba3
Shrihari Maheshwari <shrihari.maheshwari7@gmail.com>
2022-12-26 12:26:43 +0530

    Added changes the TextUtils app

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -28,6 +28,7 @@ export default function TextForm(props) {
         var text = document.getElementById("myBox")
         text.select()
         navigator.clipboard.writeText(text.value)
+        document.getSelection().removeAllRanges()
         props.showAlert("Text has been copied","success")
     }
 
@@ -35,21 +36,21 @@ export default function TextForm(props) {
         return (
             <>
                 <div className="container" style = {{color :props.mode === 'dark'?'white':'#042743'}}>
-                    <h1>{props.heading}</h1>
+                    <h1 className='mb-3'>{props.heading}</h1>
                     <div className="mb-3">
                         <textarea className="form-control" value = {text} onChange = {handleOnChange} 
-                        style = {{backgroundColor :props.mode === 'dark'?'grey':'white', color :props.mode === 'dark'?'white':'#042743'}} 
+                        style = {{backgroundColor :props.mode === 'dark'?'#13466e':'white', color :props.mode === 'dark'?'white':'#042743'}} 
                         id="myBox" rows="10"></textarea>
                     </div>
-                    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To Uppercase</button>
-                    <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert To LowerCase</button>
-                    <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear The Text</button>
-                    <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy To Clipboard</button>
+                    <button disabled = {text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert To Uppercase</button>
+                    <button disabled = {text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert To LowerCase</button>
+                    <button disabled = {text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear The Text</button>
+                    <button disabled = {text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy To Clipboard</button>
                 </div>
                 <div className="container my-3" style = {{color :props.mode === 'dark'?'white':'#042743'}}>
                     <h2>Your Text Summary</h2>
-                    <p>It has {text.split(" ").length} words and {text.length} characters</p>
-                    <p>{0.008 * text.split(" ").length} minutes to read</p>
+                    <p>It has {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+                    <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read</p>
                 </div>
             </>
         )
